efc46b1f8e5be1a1e20f44a675f3f2d6c0d56b07
cotabas <cotabas@gmail.com>
2022-11-05 07:54:34 -0700

    comments are working

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -21,11 +21,12 @@ class CommentsController < ApplicationController
 
   # POST /comments or /comments.json
   def create
-    @comment = Comment.new(comment_params)
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(comment_params)
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to comment_url(@comment), notice: "Comment was successfully created." }
+        format.html { redirect_to post_url(@post), notice: "Comment was successfully created." }
         format.json { render :show, status: :created, location: @comment }
       else
         format.html { render :new, status: :unprocessable_entity }
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,13 @@
 Rails.application.routes.draw do
-  resources :comments
-  resources :posts
-#  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
   root 'static#index'
+
+  resources :posts do
+    resources :comments
+  end
 
   devise_for :users, controllers: {
     registrations: 'users/registrations'
-#    sessions: 'users/sessions'
   }
-
 
 end
