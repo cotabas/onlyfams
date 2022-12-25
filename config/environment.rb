d8453f3dea04842c70bc23ea488ca4767c17e87c
Ubuntu <ubuntu@cptmo.dev>
2022-12-25 04:24:09 +0000

    fixed the index order and got everything working to deploy on aws

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,8 @@
 # Load the Rails application.
 require_relative "application"
 
+Rails.application.config.hosts << "onlyfams.photos"
+
 # Initialize the Rails application.
 Rails.application.initialize!
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,12 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+
+  config.action_mailer.default_url_options = { host: 'onlyfams.photos'}
+
+
+#  config.assets.unknown_asset_fallback = false
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -28,7 +34,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true 
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
